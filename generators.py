608c817a7420a6734b90d9b9d8135935a8ff5e1a
Akhilesh Aji <akhiaji@gmail.com>
2016-12-05 08:59:15 -0500

    fixed writing to file and bugs

--- generators.py
+++ generators.py
@@ -2,7 +2,7 @@ import random
 
 class RandomGenerator():
 
-    def __init__(self, size=100, density=0.5):
+    def __init__(self, size=100, density=025):
         self.size = size
         self.density = density
 
@@ -14,13 +14,14 @@ class RandomGenerator():
                     edges.append((i, j))
         random.shuffle(edges)
         if file is not None:
+            file = open(file, 'w')
             for i in edges:
                 file.write(i)
             file.close()
 
 
 class ColoredGenerator():
-    def __init__(self, size = 100, density=0.5, colors=5):
+    def __init__(self, size = 100, density=0.2, colors=5):
         self.size = size
         self.density = density
         self.colors = colors
@@ -33,37 +34,39 @@ class ColoredGenerator():
                     edges.append((i, j))
         random.shuffle(edges)
         if file is not None:
+            file = open(file, 'w')
             for i in edges:
                 file.write(str(i))
             file.close()
 
 class CliqueGenerator():
-    def __init__(self, size = 100, density=0.5, cliqueSize=5):
+    def __init__(self, size = 100, density=0.2, cliqueSize=5):
         self.size = size
         self.density = density
         self.cliqueSize = cliqueSize
 
     def generate(self, file=None):
-        edges = []
-        clique = {}
+        edges = set()
+        clique = set()
         for i in range(self.cliqueSize):
             vertex = int(random.random() * self.size)
             while vertex in clique:
                 vertex = int(random.random() * self.size)
             clique.add(vertex)
         clique = list(clique)
-        for i in range(self.cliqueSize):
-            for j in range(i, self.cliqueSize):
-                edges.append((i, j))
-
+        for i in clique:
+            for j in clique:
+                if i != j and i < j:
+                    edges.add((i, j))
         for i in range(self.size):
             for j in range(i, self.size):
                 if i%self.cliqueSize != j%self.cliqueSize and random.random() <= self.density:
-                    edges.append((i, j))
-        random.shuffle(edges)
+                    edges.add((i, j))
+        random.shuffle(list(edges))
         if file is not None:
+            file = open(file, 'w')
             for i in edges:
-                file.write(str(i))
+                file.write(str(i[0]) + ", " + str(i[1]) + "\n")
             file.close()
         else:
             for i in edges:
